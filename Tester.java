c30fdf42b85901a0eba6c408cecf6f15a885c250
Marcus T <mtwyford@users.noreply.github.com>
2022-01-10 09:34:35 -0500

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,15 @@
+public class Tester {
+
+	public static void main(String[] args) {
+
+    SortingAlgorithms s1 = new SortingAlgorithms;
+    //create an int[] of length 100;
+    //  private int[] arr = int[100];
+    //									{x,y,z,....};
+    s1.bubbleSort();
+    s1.insertionSort();  //print # swaps for each
+    s1.selectionSort();
+	
+	}
+
+}
