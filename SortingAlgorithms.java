8d89007e3e8e5c44f376070b337dce037a1c811b
Daniel Yi <77285801+yidan23@users.noreply.github.com>
2022-01-12 08:58:21 -0500

    updated swap and a bit of insertion sort

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,5 +1,5 @@
 public class SortingAlgorithms{
-	private int[] array = {10, 9, 8, 7 , 6, 5, 4, 3, 2, 1};
+	private int[] array = {95,85,89,69,76,84,86,65,52,86,46,65,45,59,76,66,22,1,91,86,14,6,68,31,57,50,90,80,8,87,42,76,48,35,91,73,50,9,56,8,76,17,64,3,30,99,5,26,4,24,44,92,63,84,48,58,56,33,13,31,10,90,13,48,24,93,64,60,49,84,76,69,48,12,81,61,58,9,19,34,61,34,58,89,4,78,55,58,30,58,73,36,11,55,14,60,63,48,53,41 };
 	
 	public void bubbleSort(){
 		int[] arr = new int[array.length];
@@ -64,10 +64,10 @@ public class SortingAlgorithms{
 			int k = i;
 			while(k> 0 && arr[k-1] > temp){
 				arr[k] = arr[k-1];
+				arr[k-1] = temp;
 				k--;
 				swaps++;
 			}
-			arr[k] = temp;
 		}
 		System.out.println("Insertion Sort Swaps: " + swaps);
 	}
